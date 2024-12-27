97380e36de70b3179f40dc16987bef86d802778a
Dilanka Gunarathna <dilanka.gunarathna@zebra.com>
2024-12-27 16:12:32 +0530

    initial commit message

---
+++ src/main/java/com/example/demo_gradle/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.demo_gradle;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/***********************************************
+ * CONFIDENTIAL AND PROPRIETARY
+ *
+ * The information contained herein is the confidential and the exclusive property of
+ * Zebra Technologies Corporation. This document, and the information contained herein, shall not be copied, reproduced, published,
+ * displayed or distributed, in whole or in part, in any medium, by any means, for any purpose without the express
+ * written consent of Zebra Technologies Corporation.
+ *
+ * ZEBRA and the stylized Zebra head are trademarks of Zebra Technologies Corporation, registered in many jurisdictions worldwide.
+ * All other trademarks are the property of their respective owners.
+ * 2024 Zebra Technologies Corporation and/or its affiliates.  All rights reserved.
+ *
+ * @created 27/12/2024 : 11:42 AM
+ * @author DG4285
+ ***********************************************/
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, Spring Boot!";
+    }
+}
